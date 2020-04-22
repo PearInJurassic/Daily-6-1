2491cf4bc3d79e430724171e639600c089b8685a
PearInJurassic <aassd123lowe@outlook.com>
2020-04-22 11:16:52 +0800

    vue-cli 4.3.1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import navBar from './components/nav-bar'
+export default {
+  name: 'App',
+  components: {
+    navBar,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "navBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
