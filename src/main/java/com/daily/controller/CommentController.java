0a027c2e55c34d21d39df037108ab1194d9c8cf1
muxiao167 <60741631+muxiao167@users.noreply.github.com>
2020-04-29 20:20:20 +0800

    完成CommentController类编写

--- src/main/java/com/daily/controller/CommentController.java
+++ src/main/java/com/daily/controller/CommentController.java
@@ -3,12 +3,17 @@ package com.daily.controller;
 import com.daily.entity.Area;
 import com.daily.entity.Comment;
 import com.daily.service.CommentService;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,9 +32,83 @@ public class CommentController {
     private Map<String, Object> getCommentByCommentId(int commentId) {
         Map<String, Object> modelMap = new HashMap<String, Object>();
         Comment comment = new Comment();
-        // 获取地区信息
+        // 获取评论信息
         comment = commentService.getCommentByCommentId(commentId);
-        modelMap.put("area", comment);
+        modelMap.put("comment", comment);
+        return modelMap;
+    }
+
+    /*
+     * 通过帖子ID得到评论列表
+     *
+     * @param postId
+     * @return
+     */
+    @RequestMapping(value = "/getcommentbypostid", method = RequestMethod.GET)
+    private Map<String, Object> getCommentByPostId(int postId) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        List<Comment> commentList = new ArrayList<Comment>();
+        // 获取评论列表
+        commentList = commentService.getCommentByPostId(postId);
+        modelMap.put("commentList", commentList);
+        return modelMap;
+    }
+
+    /*
+     * 通过用户ID得到评论列表
+     *
+     * @param postId
+     * @return
+     */
+    @RequestMapping(value = "/getcommentbyuserid", method = RequestMethod.GET)
+    private Map<String, Object> getCommentByUserId(int userId) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        List<Comment> commentList = new ArrayList<Comment>();
+        // 获取评论列表
+        commentList = commentService.getCommentByUserId(userId);
+        modelMap.put("commentList", commentList);
+        return modelMap;
+    }
+
+    /*
+     * 新增评论
+     *
+     * @param Comment
+     * @return boolean
+     */
+    @RequestMapping(value = "/createcomment", method = RequestMethod.POST)
+    private Map<String, Object> createComment(@RequestBody Comment comment) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        // 新增评论
+        modelMap.put("success", commentService.createComment(comment));
+        return modelMap;
+    }
+
+    /*
+     * 修改评论
+     *
+     * @param Comment
+     * @return boolean
+     */
+    @RequestMapping(value = "/updatecomment", method = RequestMethod.POST)
+    private Map<String, Object> updateComment(@RequestBody Comment comment) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        // 修改评论
+        modelMap.put("success", commentService.updateComment(comment));
+        return modelMap;
+    }
+
+    /*
+     * 删除评论
+     *
+     * @param Comment
+     * @return boolean
+     */
+    @RequestMapping(value = "/deletecomment", method = RequestMethod.GET)
+    private Map<String, Object> deleteComment(int commentId) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        // 删除评论
+        modelMap.put("success", commentService.deleteComment(commentId));
         return modelMap;
     }
 }
