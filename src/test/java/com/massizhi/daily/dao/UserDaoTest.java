c5a5f8f979c9beb71223d4f192da600b83dc9ce8
massizhi <2750201946@qq.com>
2020-04-30 18:49:52 +0800

    feat add junit:UserDaoTest

---
+++ src/test/java/com/massizhi/daily/dao/UserDaoTest.java
@@ -0,0 +1,20 @@
+package com.massizhi.daily.dao;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class UserDaoTest {
+
+    @Test
+    void getUserByUserId() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+}
