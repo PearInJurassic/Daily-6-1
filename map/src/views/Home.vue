c4cbd66fbf7766448ad3fa64607b31c284a8ccc9
LinLing1027 <910091277@qq.com>
2020-05-08 17:39:13 +0800

    map
    地图接口更新

---
+++ map/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <country></country>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import country from '@/components/country.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    country
+  }
+}
+</script>
