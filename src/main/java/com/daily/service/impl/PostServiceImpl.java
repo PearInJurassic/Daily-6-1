e21db4a0d45b5e844c9409f98883585b36b5b4a1
massizhi <2750201946@qq.com>
2020-05-31 18:24:15 +0800

    feat 合并陈炎的修改

--- src/main/java/com/daily/service/impl/PostServiceImpl.java
+++ src/main/java/com/daily/service/impl/PostServiceImpl.java
@@ -257,7 +257,10 @@ public class PostServiceImpl implements PostService {
         for(Post post : postList) {
             long t = post.getPostCreateTime().getTime();
             t = now.getTime() - t;
-            time = (int) t / 1000000;
+            if( t > 20 * 1000 * 3600 * 24 ) { //超过20天
+                t = 20 * 1000 * 3600 * 24;
+            }
+            time = (int) t / 10000000;
             forwardNum = post.getForwardNum();
             tipoffNum = post.getTipoffNum();
             likeNum = likeDao.queryLikeNumByPostId(post.getPostId());
